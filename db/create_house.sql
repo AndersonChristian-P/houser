09ff5a4c5e403a618c70172389de9212a2b9b30d
Christian Anderson <ca96187@gmail.com>
2019-04-26 10:06:08 -0600

    completed Database

---
+++ db/create_house.sql
@@ -0,0 +1,19 @@
+INSERT INTO houses (
+  name,
+  address,
+  city,
+  state,
+  zip,
+  img,
+  mortgage,
+  rent
+) VALUES (
+  'Christian',
+  '2359 Furnace Rd',
+  'Felton',
+  'PA',
+  17322,
+  'https://pmcvariety.files.wordpress.com/2018/07/bradybunchhouse_sc11.jpg?w=1000&h=563&crop=1',
+  235.15,
+  100.26
+);
--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
+require("dotenv").config()
 const express = require("express")
+const massive = require("massive")
 
 const app = express()
 const port = 4001
+const { CONNECTION_STRING } = process.env
 
 const ctrl = require("./controller")
 
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db)
+  console.log("db is connected")
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+  })
+})
+
+// --Middleware--
 app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
