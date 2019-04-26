ca44c81fec674249fd421f3a244726e4df0de247
Christian Anderson <ca96187@gmail.com>
2019-04-26 10:28:53 -0600

    completed Part 1 Step 2

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -5,13 +5,70 @@ export default class Wizard extends Component {
     super()
 
     this.state = {
-
+      name: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: 0
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    let { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state)
   }
 
   render() {
     return (
-      <div>Wizard</div>
+      <div>
+
+        <div>Property Name</div>
+        <input
+          onChange={this.handleChange}
+          name="name"
+          value={this.state.name}
+          type="text"
+        />
+
+        <div>Address</div>
+        <input
+          onChange={this.handleChange}
+          name="address"
+          value={this.state.address}
+          type="text"
+        />
+
+        <div>City</div>
+        <input
+          onChange={this.handleChange}
+          name="city"
+          value={this.state.city}
+          type="text"
+        />
+
+        <div>State</div>
+        <input
+          onChange={this.handleChange}
+          name="state"
+          value={this.state.state}
+          type="text"
+        />
+
+        <div>Zip</div>
+        <input
+          onChange={this.handleChange}
+          name="zip"
+          value={this.state.zip}
+          type="number"
+        />
+
+
+      </div>
     )
   }
 }
