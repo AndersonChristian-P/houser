6a464ee82ec9e3d45f949047f0e6ae0a8946162f
Christian Anderson <ca96187@gmail.com>
2019-04-26 09:42:06 -0600

    completed React setup

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import House from "./../House/House"
+export default class Dashboard extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Dashboard
+        <House />
+      </div>
+
+    )
+  }
+}
